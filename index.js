d974e1782d8775389867f2ffc5b73877fc6d585e
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-03-20 22:31:16 +0700

    User crud without using DB

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 import express from 'express';
-
+import { users,books,teachers } from './simple.js';
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-function getAllUsers(req,res){
-    return res.json({message: "All users"});
-}
+app.get('/api/users',(req, res) => {
+    return res.json(users);
+});
+app.get('/api/books',(req,res) => {
 
-app.get('/users', getAllUsers);
+    return res.json(books);
+})
+app.get('/api/books/:id',(req,res) => {
+    const id = parseInt(req.params.id)
+    const book = books.find((b) => {
+        return b.id === id
+    })
+    if(!book){
+        return res.status(404).json({message : "Not found"})
+    }
+    return res.json(book)
+})
+
+app.post('/api/users',(req,res) => {
+    users.push(req.body)
+
+    return res.status(201).json({message : `Users with ${req.body.name} created`});
+})
+
+app.patch('/api/users',(req,res) => {
+    const userId = req.params.id;
+    const userIndex = users.findIndex((u) => {
+        return userId == u.id
+    })
+    users[userIndex] = {id: userId,...req.body}
+    return res.json(({message: `User with id  ${userId} updated`}))
+
+})
+app.get('/api/users/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find((u) => {
+        return u.id ===  id
+    });
+    if(!user){
+        return res.status(404).json({message: "Not Found"});
+    }
+    return res.json(user)
+});
+
+app.delete('/api/users/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const deleteIndex = users.findIndex((u) => {
+        return u.id === id
+    })
+    if(deleteIndex === -1){
+        return res.status(404).json({message: "Not Found"})
+    }
+    users.splice(deleteIndex,1)
+    return res.json({message: `User with ${id} deleted`});
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ simple.js
@@ -0,0 +1,18 @@
+const books = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { id: 3, title: '1984', author: 'George Orwell' },
+];
+
+const users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+]
+const teachers = [
+    { id: 1, name: 'Mr. Smith', subject: 'Math' },
+    { id: 2, name: 'Ms. Johnson', subject: 'English' },
+    { id: 3, name: 'Mrs. Brown', subject: 'Science' }
+]
+
+export { books, users, teachers };
